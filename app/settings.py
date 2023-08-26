73775e3b63afc70cb03be3094622572bb6022438
gal-globerman <galgloberman@outlook.com>
2023-08-26 19:22:36 +0200

    refactor: allow auto gen rsa key

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,12 @@
 from functools import cached_property
 
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 from pydantic import PostgresDsn, RedisDsn, SecretStr, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+public_key = private_key.public_key()
 
 
 class AppSettings(BaseSettings):
@@ -9,8 +14,19 @@ class AppSettings(BaseSettings):
 
     DATABASE_DSN: PostgresDsn
     REDIS_DSN: RedisDsn
-    PUBLIC_KEY: SecretStr
-    PRIVATE_KEY: SecretStr
+    PUBLIC_KEY: SecretStr = SecretStr(
+        public_key.public_bytes(
+            serialization.Encoding.PEM,
+            serialization.PublicFormat.SubjectPublicKeyInfo,
+        ).decode()
+    )
+    PRIVATE_KEY: SecretStr = SecretStr(
+        private_key.private_bytes(
+            serialization.Encoding.PEM,
+            serialization.PrivateFormat.TraditionalOpenSSL,
+            serialization.NoEncryption(),
+        ).decode()
+    )
     MAX_MATCHING_TIMEOUT: float = 6.7
     JWT_LIFETIME: int = 3600
 
